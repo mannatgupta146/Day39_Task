9d3a6161fdcfe58f820e8b5c54739cae7ffbd922
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-11 23:47:21 +0530

    questions added

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // Level 1 - Basic Function, Array and Object Code Tasks (Easy)
+
+// 1. Write a function `sayHello()` that prints `Hello JavaScript`.
+
+
+// 2. Create a function `add(a, b)` that returns their sum and log the result.
+
+
+// 3. Write a function with a default parameter `name = "Guest"` that prints `Hi <name>`.
+
+
+// 4. Use rest parameters to make a function that adds unlimited numbers.
+
+
+// 5. Create an IIFE that prints `I run instantly!`.
+
+
+// 6. Make a nested function where the inner one prints a variable from the outer one.
+
+
+// 7. Create an array of 5 fruits. Add one at the end and remove one from the beginning.
+
+
+// 8. Use a `for` loop to print all elements of an array.
+
+
+// 9. Create an object `person` with keys `name`, `age`, and `city`, and print each key’s value.
+
+
+// 10. Use `setTimeout()` to log `Time’s up!` after 2 seconds.
+
+
