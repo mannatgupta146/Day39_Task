debc770d2c48a23c178499cd5458f4fecfc244e3
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-12 14:31:33 +0530

    6 done

--- script.js
+++ script.js
@@ -1,40 +1,5 @@
-// Level 1 - Basic Function, Array and Object Code Tasks (Easy)
 
-// 1. Write a function `sayHello()` that prints `Hello JavaScript`.
-function sayHello(){
-    console.log("Hello Javascript")
-}
-sayHello()
 
-// 2. Create a function `add(a, b)` that returns their sum and log the result.
-function add(a, b){
-    let sum = a+b;
-    console.log(sum)
-}
-add(5,7)
-
-// 3. Write a function with a default parameter `name = "Guest"` that prints `Hi <name>`.
-function guest(name = "Guest"){
-    console.log(`Hi ${name}`)
-}
-guest()
-
-// 4. Use rest parameters to make a function that adds unlimited numbers.
-function unlimitedNumbers(...nums){
-    let sum = 0
-    nums.forEach(num =>{
-        sum+=num
-    })
-    console.log(sum);
-}
-unlimitedNumbers(1,2,3,4,5,6)
-
-// 5. Create an IIFE that prints `I run instantly!`.
-(function(){
-    console.log("I run instantly!");
-})()
-
-// 6. Make a nested function where the inner one prints a variable from the outer one.
 
 
 // 7. Create an array of 5 fruits. Add one at the end and remove one from the beginning.
