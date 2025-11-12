5c26b8ef3a6a5bebbeda395b5b8466656cce2486
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-12 18:42:47 +0530

    completed

--- script.js
+++ script.js
@@ -1,9 +1,81 @@
+// Level 1 - Basic Function, Array and Object Code Tasks (Easy)
 
+// 1. Write a function `sayHello()` that prints `Hello JavaScript`.
+function sayHello(){
+    console.log("Hello Javascript")
+}
+sayHello()
+
+
+// 2. Create a function `add(a, b)` that returns their sum and log the result.
+function add(a, b){
+    let sum = a+b;
+    console.log(sum)
+}
+add(5,7)
+
+
+// 3. Write a function with a default parameter `name = "Guest"` that prints `Hi <name>`.
+function guest(name = "Guest"){
+    console.log(`Hi ${name}`)
+}
+guest()
+
+
+// 4. Use rest parameters to make a function that adds unlimited numbers.
+function unlimitedNumbers(...nums){
+    let sum = 0
+    nums.forEach(num =>{
+        sum+=num
+    })
+    console.log(sum);
+}
+unlimitedNumbers(1,2,3,4,5,6)
+
+
+// 5. Create an IIFE that prints `I run instantly!`.
+(function(){
+    console.log("I run instantly!");
+})()
+
+
+// 6. Make a nested function where the inner one prints a variable from the outer one.
+function outer(a){
+    function inner(){
+        console.log(a)
+    }
+    inner()
+}
+outer(5)
+
+
+// 7. Create an array of 5 fruits. Add one at the end and remove one from the beginning.
+let arr = ["mango", "pineapple", "orange", "grapes", "kiwi"]
+arr.shift()
+arr.push("apple")
+console.log(arr)
+
+
+// 8. Use a `for` loop to print all elements of an array.
+let ar = [1, 2, 3, 4, 5, 6]
+for(let i = 0; i<ar.length; i++){
+    console.log(ar[i])
+}
 
 
 // 9. Create an object `person` with keys `name`, `age`, and `city`, and print each key’s value.
+let obj ={
+    name: "mannat",
+    age: "20",
+    city: "udhampur"
+}
+
+for(let key in obj){
+    console.log(obj[key])
+}
 
 
 // 10. Use `setTimeout()` to log `Time’s up!` after 2 seconds.
-
-
+setTimeout(function(){
+    console.log("Time’s up!")
+}, 2000)
